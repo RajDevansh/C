d9d8b4a4a0666ff38150b53093ee2319c7862757
RajDevansh <devanshrajpoot2008@gmail.com>
2025-08-06 22:14:40 +0530

    Add files via upload

---
+++ Datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=1;
+    char b='a';
+    float c=12.2f;
+    double d=13.29;
+    printf("%d\n",a);
+    printf("%c\n",b);
+    printf("%f\n",c);
+    printf("%lf\n",d);
+    return 0;
+}
