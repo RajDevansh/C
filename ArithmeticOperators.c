3707347fb993dde9cfbf4b3ea7f366077ba07bac
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-21 19:59:42 +0530

    Add files via upload

---
+++ ArithmeticOperators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=2;
+    printf("Addition=%d",a+b);
+    printf("Substraction=%d",a-b);
+    printf("Multiply=%d",a*b);
+    printf("Division=%d",a/b);
+    printf("Modulus=%d",a%b);
+    return 0;  
+}
