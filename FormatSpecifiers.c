a96ba3138759d97c3bc81fad8aaacc878bac8115
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-24 19:11:16 +0530

    Add files via upload

---
+++ FormatSpecifiers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    float b=5.5;
+    char c='A';
+    printf("Integer value=%d\n",a);
+    printf("Float value=%.2f\n",b);
+    printf("Character value=%c\n",c);
+    return 0;
+}
