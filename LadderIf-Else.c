6e397b10fe4a14419f6ceeee9806b3083feb3520
RajDevansh <devanshrajpoot2008@gmail.com>
2025-09-26 22:37:52 +0530

    Add files via upload

---
+++ LadderIf-Else.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=20;
+    if(a>b){
+        printf("A is greater than B");
+    }else if(a<b){
+        printf("A is smaller than B");
+    }
+}
